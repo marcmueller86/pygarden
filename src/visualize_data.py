c5b8c31f1d805dc934cb15a38c676544ee7f845f
marcmueller86 <marcmuellerde@gmail.com>
2020-06-12 01:04:26 +0200

    added index with out refresh

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -4,15 +4,22 @@ import plotly.io as pio
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.figure_factory as ff
+from datetime import datetime, timedelta
 
 class VisualizeData(object):
 
     def create_export(self, file_name):
         df = pd.read_csv(file_name)
+        df['name'][5]='sensor_2'
+        df['name'][15]='sensor_2'
+        df['name'][19]='sensor_2'
 
         df['datetime'] = pd.to_datetime(df['date_iso'])
-        df_sample = df[['datetime','name','temperature','light','battery','moisture','conductivity']].resample(rule='15Min', on='datetime').mean().dropna()
-        df = df_sample.reset_index()
+        df['datetime_rounded']=df.datetime.apply(lambda x: self.round_to_hour(x))
+        df_resampled = df.groupby(by=['datetime_rounded','name'], axis=0).mean().reset_index()
+
+        #df_sample = df[['datetime','name','temperature','light','battery','moisture','conductivity']].resample(rule='15Min', on='datetime').mean().dropna()
+        df = df_resampled.reset_index()
         fig = make_subplots(
             rows=3, cols=2,
             shared_xaxes=True,
@@ -22,26 +29,27 @@ class VisualizeData(object):
                 [{"type": "scatter"},
                 {"type": "scatter"}],
                 [{"type": "table","colspan": 2}, None]],
-                subplot_titles=("Temperatur","Licht", "Feuchtigkeit","Leitfähigkeit", "Zusammenfassung"),
+                subplot_titles=("Temperatur","Licht", "Feuchtigkeit","Leitfähigkeit"),
                 horizontal_spacing = 0.05
         )
-
-        fig = self.create_scatter_trace(df, fig, 'temperature', 'Temperatur', 1, 1)
-        fig = self.create_scatter_trace(df, fig, 'moisture', 'Feuchtigkeit', 1, 2)
-        fig = self.create_scatter_trace(df, fig, 'light', 'Licht', 2, 1)
-        fig = self.create_scatter_trace(df, fig, 'conductivity', 'Leitfähigkeit', 2, 2)
+        for sensor in df_resampled['name'].unique():
+            df_sensor = df[df['name'] == sensor] 
+            fig = self.create_scatter_trace(df_sensor, fig, 'temperature', sensor, 1, 1)
+            fig = self.create_scatter_trace(df_sensor, fig, 'moisture', sensor, 1, 2)
+            fig = self.create_scatter_trace(df_sensor, fig, 'light', sensor, 2, 1)
+            fig = self.create_scatter_trace(df_sensor, fig, 'conductivity', sensor, 2, 2)
 
         # fig.add_trace(
         #     go.Scatter(x=df['datetime'], y=df['battery'],name='Batterie Ladung'),
         #     row=3, col=1
         # )
         decimals = 2
-        df = df.sort_values(by='datetime', ascending=False).head(5)
+        df = df.sort_values(by='datetime_rounded', ascending=False).head(5)
         fig.add_trace(
             go.Table(
-            header=dict(values=['Datum','Leitfähigkeit(us/cm)', 'Lichtstärke (Lux)', 'Feuchtigkeit (%)', 'Temperatur (C°)', 'Batterie Ladung (%)'],
+            header=dict(values=['Datum','Leitfähigkeit(us/cm)', 'Lichtstärke (Lux)', 'Feuchtigkeit (?%)', 'Temperatur (C°)', 'Batterie Ladung (%)'],
                         align='left'),
-            cells=dict(values=[df.datetime, df.conductivity.apply(lambda x: round(x, decimals)), df.light.apply(lambda x: round(x, decimals)), df.moisture*100, df.temperature.apply(lambda x: round(x, decimals)), df.battery],
+            cells=dict(values=[df.datetime_rounded, df.conductivity.apply(lambda x: round(x, decimals)), df.light.apply(lambda x: round(x, decimals)), df.moisture, df.temperature.apply(lambda x: round(x, decimals)), df.battery],
                     align='left')),
                     row=3, col=1
         )
@@ -57,10 +65,16 @@ class VisualizeData(object):
 
     def create_scatter_trace(self, df, fig, y, name, row, col):
         fig.add_trace(
-            go.Scatter(x=df['datetime'], y=df[y],name=name),
+            go.Scatter(x=df['datetime_rounded'], y=df[y],name=name),
             row=row, col=col
         )
         return fig
+
+    def round_to_hour(self, t):
+        # Rounds to nearest hour by adding a timedelta hour if minute >= 30
+        return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour)
+                    +timedelta(hours=t.minute//30))
+
 
 if __name__ == '__main__':
     vd = VisualizeData()
