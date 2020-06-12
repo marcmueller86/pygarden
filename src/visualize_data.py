872a98297060a7674d3c085bcb201f9e62fa15ca
marcmueller86 <marcmuellerde@gmail.com>
2020-06-12 09:23:06 +0200

    removing rounding

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -16,15 +16,15 @@ class VisualizeData(object):
         df['name'][19]='sensor_2'
 
         df['datetime'] = pd.to_datetime(df['date_iso'])
-        df['datetime_rounded']=df.datetime.apply(lambda x: self.round_to_hour(x))
-        df_resampled = df.groupby(by=['datetime_rounded','name'], axis=0).mean().reset_index()
+        # df['datetime_rounded']=df.datetime.apply(lambda x: self.round_to_hour(x))
+        # df_resampled = df.groupby(by=['datetime_rounded','name'], axis=0).mean().reset_index()
 
         #df_sample = df[['datetime','name','temperature','light','battery','moisture','conductivity']].resample(rule='15Min', on='datetime').mean().dropna()
-        df = df_resampled.reset_index()
+        # df = df_resampled.reset_index()
         fig = make_subplots(
             rows=3, cols=2,
             shared_xaxes=True,
-            vertical_spacing=0.15,
+            vertical_spacing=0.125,
             specs=[[{"type": "scatter"},
                 {"type": "scatter"}],
                 [{"type": "scatter"},
@@ -36,7 +36,7 @@ class VisualizeData(object):
         # Only a few colors, add more if you need more than 10 sensors
         cols = plotly.colors.DEFAULT_PLOTLY_COLORS
 
-        for i, sensor in enumerate(df_resampled['name'].unique()):
+        for i, sensor in enumerate(df['name'].unique()):
             df_sensor = df[df['name'] == sensor] 
             fig = self.create_scatter_trace(cols[i], df_sensor, fig, 'temperature', sensor, 1, 1, True)
             fig = self.create_scatter_trace(cols[i], df_sensor, fig, 'moisture', sensor, 1, 2, False)
@@ -48,12 +48,12 @@ class VisualizeData(object):
         #     row=3, col=1
         # )
         decimals = 2
-        df = df.sort_values(by='datetime_rounded', ascending=False).head(10)
+        df = df.sort_values(by='datetime', ascending=False).head(10)
         fig.add_trace(
             go.Table(
             header=dict(values=['Sensor','Datum','Leitfähigkeit(us/cm)', 'Lichtstärke (Lux)', 'Feuchtigkeit (?%)', 'Temperatur (C°)', 'Batterie Ladung (%)'],
                         align='left'),
-            cells=dict(values=[df.name, df.datetime_rounded, df.conductivity.apply(lambda x: round(x, decimals)), df.light.apply(lambda x: round(x, decimals)), df.moisture.apply(lambda x: round(x, decimals)), df.temperature.apply(lambda x: round(x, decimals)), df.battery],
+            cells=dict(values=[df.name, df.datetime, df.conductivity.apply(lambda x: round(x, decimals)), df.light.apply(lambda x: round(x, decimals)), df.moisture.apply(lambda x: round(x, decimals)), df.temperature.apply(lambda x: round(x, decimals)), df.battery],
                     align='left')),
                     row=3, col=1
         )
@@ -69,15 +69,15 @@ class VisualizeData(object):
 
     def create_scatter_trace(self, color, df, fig, y, name, row, col, legend):
         fig.add_trace(
-            go.Scatter(x=df['datetime_rounded'], y=df[y],name=name, line=dict(width=2, color=color),showlegend=legend),
+            go.Scatter(x=df['datetime'], y=df[y],name=name, line=dict(width=2, color=color),showlegend=legend),
             row=row, col=col
         )
         return fig
 
-    def round_to_hour(self, t):
-        # Rounds to nearest hour by adding a timedelta hour if minute >= 30
-        return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour)
-                    +timedelta(hours=t.minute//30))
+    # def round_to_hour(self, t):
+    #     # Rounds to nearest hour by adding a timedelta hour if minute >= 30
+    #     return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour)
+    #                 +timedelta(hours=t.minute//30))
 
 
 if __name__ == '__main__':
